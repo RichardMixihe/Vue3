23f4f11a2b4ba313964ee8fdb5ed365977b6a944
Richard <richardmixihe@gmail.com>
2022-12-14 16:23:03 +0800

    setup函数的作用-响应式数据的定义方式1-ref

---
+++ src/03-setup-函数的作用.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  // setup 函数的执行时机和具体使用
+  setup() {
+    console.log('setup 执行');
+    // 定义组件需要用的数据和函数
+    let msg = 'hello vue3';
+    const sayHi = () => {
+      console.log('前', msg);
+      msg = 'hello vue4';
+      console.log('后', msg);
+    };
+
+    return { msg, sayHi };
+  },
+
+  beforeCreate() {
+    console.log('组件before create');
+  },
+};
+</script>
+
+<template>
+  <h2>{{ msg }}</h2>
+  <button @click="sayHi">按钮</button>
+</template>
+
+<style scoped></style>
--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,24 @@
 <script>
 import { ref } from 'vue';
 export default {
+  // 在 setup 中定义响应式数据
   setup() {
-    // 用什么创建什么。就像你写函数，用变量定义。用函数你定义。
-    // 定义表示图片隐藏现实的标志变量
-    const show = ref(true);
-    // 定义函数，用来改变 show，改变页面状态
-    const toggle = () => {
-      show.value = !show.value;
+    // 定义一个age响应式数据
+    const age = ref(18);
+    // 修改数据的函数
+    const decrement = () => {
+      age.value = age.value - 1;
     };
 
-    // 计数器
-    const count = ref(0);
-    // 改变count的函数
-    const increment = () => {
-      count.value += 1;
-    };
-
-    // 把你定义的变量和函数返回给vue组件
-    return { show, toggle, count, increment };
+    return { age, decrement };
   },
 };
 </script>
 
 <template>
-  <img v-if="show" alt="Vite logo" src="/vite.svg" />
-  <button @click="toggle">显示隐藏图片</button>
-  <hr />
-  <h2>计数器：{{ count }}</h2>
-  <button @click="increment">累加</button>
+  <h2>{{ age }}</h2>
+  <button @click="age++">+</button>
+  <button @click="decrement">-</button>
 </template>
 
 <style scoped></style>
